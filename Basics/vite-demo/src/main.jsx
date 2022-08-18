4393dc07fd145ca92513d40042ef1953c5b27130
CSC108 <st.d447@gmail.com>
2022-08-18 17:39:39 +0530

    useMemo Practice Practice/userProfile

--- Basics/vite-demo/src/main.jsx
+++ Basics/vite-demo/src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import TakePhoto from './Projects/takePhoto/takePhoto_class'
-import UserProfile from './Practice/userProfile/UserProfile'
+import TakePhoto from "./Projects/takePhoto/takePhoto_class";
+import UserProfile from "./Practice/userProfile/UserProfile";
+import UserProfile02 from "./Practice/userProfile/UserProfile02";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <UserProfile />
- 
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<UserProfile02 />);
